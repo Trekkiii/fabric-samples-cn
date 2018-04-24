023b1930874309fabbcc46d498bbb67dd889528c
liuchunlong <631521383@qq.com>
2018-04-24 11:57:57 +0800

    更新项目结构

--- README.md
+++ README.md
@@ -1,3 +1,113 @@
-# fabric-shell
+# fabric-shell v1.1.0
 
-务必保证`Clone`该项目至与`fabric`源码处于同一级目录。
+## 前言
+
+### 安装GO
+
+下载地址:
+
+https://studygolang.com/dl
+
+拷贝到`/usr/local`目录下解压
+
+```text
+tar -zxvf go1.10.1.linux-amd64.tar.gz
+```
+
+在`$HOME`下新建`gopath`目录，并且新增`src`子目录。
+
+```text
+cd src/
+
+mkdir github.com
+
+cd github.com
+
+mkdir hyperledger
+```
+
+编辑/etc/profile文件:
+
+```text
+export GOPATH=$HOME/gopath
+export GOROOT=/usr/local/go
+export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
+```
+
+### 下载源码
+
+将fabric源码Clone到hyperledger目录下。
+
+```text
+git clone https://github.com/hyperledger/fabric.git
+git checkout -b v1.1.0 v1.1.0
+```
+
+然后将该代码Clone到到hyperledger目录下。
+
+```text
+git@github.com:fnpac/fabric-shell.git
+```
+
+此时的目录结构应该是这样子：
+
+```text
+root@vm***:~/gopath/src/github.com/hyperledger# ll
+total 20
+drwxr-xr-x  5 root   root   4096 Apr 24 11:26 ./
+drwxr-xr-x  3 root   root   4096 Apr 17 18:12 ../
+drwxr-xr-x 28 root   root   4096 Apr 18 14:37 fabric/
+drwxrwxr-x  6 root   root   4096 Apr 24 11:25 fabric-shell/
+```
+
+### 安装docker
+
+```text
+curl -fsSL https://get.docker.com | sh
+
+等待安装完毕，现在我们使用下面的命令启动 Docker：
+systemctl start docker
+
+重新启动
+systemctl restart docker
+
+设置开机自启动
+systemctl enable docker
+
+查看状态
+systemctl status docker
+
+查看所有已启动的服务
+systemctl list-units --type=service
+```
+
+### 安装docker-compose
+
+```text
+apt-get install python-pip
+pip install docker-compose
+```
+
+### 下载fabric docker镜像
+
+```text
+cd /root/gopath/src/github.com/hyperledger/
+
+curl -sSL https://goo.gl/6wtTN5 | bash -s 1.1.0
+```
+
+## e2e_cli
+
+该脚本基于docker-compose创建单机fabric网络。
+
+直接运行该脚本即可创建fabric网络。
+
+```text
+./network_setup.sh up
+```
+
+关闭网络执行如下
+
+```text
+./network_setup.sh down
+```
--- e2e_cli/scripts/script.sh
+++ e2e_cli/scripts/script.sh
@@ -18,8 +18,8 @@ CHANNEL_NAME="$1" # 应用通道名称
 
 : ${TIMEOUT:="60"} # 超时
 
-COUNTER=1
-MAX_RETRY=5
+COUNTER=1 # 节点加入应用通道的尝试次数
+MAX_RETRY=5 # 节点加入应用通道的最大尝试次数
 
 # Orderer TLS CA根证书，同 ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt
 ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
@@ -105,12 +105,169 @@ setGlobals () {
 
 createChannel() {
 
-    # 设置peer节点连接属性
+    # 设置peer0节点连接属性
+    # 通过peer0节点创建应用通道
     setGlobals 0
 
-    # TODO
+    # cli节点通过 /bin/bash -c './scripts/script.sh ${CHANNEL_NAME}; sleep $TIMEOUT' 执行该脚本，所以当前路径为 e2e_cli/
+    if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+        peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx >&log.txt
+    else
+		peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls --cafile $ORDERER_CA >&log.txt
+	fi
+
+    res=$?
+	cat log.txt
+
+	verifyResult $res "Channel creation failed"
+	echo "===================== Channel \"$CHANNEL_NAME\" is created successfully ===================== "
+	echo
 }
 
+# Sometimes Join takes time hence RETRY atleast for 5 times
+joinWithRetry () {
+
+    peer channel join -b $CHANNEL_NAME.block  >&log.txt
+
+    res=$?
+	cat log.txt
+
+	if [ $res -ne 0 -a $COUNTER -lt $MAX_RETRY ]; then
+	    COUNTER=`expr $COUNTER + 1`
+	    echo "PEER$1 failed to join the channel, Retry after 2 seconds"
+		sleep 2
+		joinWithRetry $1
+    else
+		COUNTER=1
+    fi
+
+    verifyResult $res "After $MAX_RETRY attempts, PEER$ch has failed to Join the Channel"
+}
+
+joinChannel () {
+
+    for ch in 0 1 2 3
+    do
+        # 设置当前节点的连接属性
+        setGlobals $ch
+        joinWithRetry $ch
+        echo "===================== PEER$ch joined on the channel \"$CHANNEL_NAME\" ===================== "
+		sleep 2
+		echo
+	done
+}
+
+updateAnchorPeers() {
+
+    PEER=$1
+    setGlobals $PEER
+
+    if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+		peer channel update -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx >&log.txt
+	else
+		peer channel update -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls --cafile $ORDERER_CA >&log.txt
+	fi
+
+	res=$?
+	cat log.txt
+
+	verifyResult $res "Anchor peer update failed"
+
+	echo "===================== Anchor peers for org \"$CORE_PEER_LOCALMSPID\" on \"$CHANNEL_NAME\" is updated successfully ===================== "
+	sleep 5
+	echo
+}
+
+installChaincode () {
+
+    PEER=$1
+	setGlobals $PEER
+
+    peer chaincode install -n mycc -v 1.0 -p github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 >&log.txt
+
+    res=$?
+	cat log.txt
+    verifyResult $res "Chaincode installation on remote peer PEER$PEER has Failed"
+	echo "===================== Chaincode is installed on remote peer PEER$PEER ===================== "
+	echo
+}
+
+instantiateChaincode () {
+
+    PEER=$1
+	setGlobals $PEER
+
+    # while 'peer chaincode' command can get the orderer endpoint from the peer (if join was successful),
+	# lets supply it directly as we know it using the "-o" option
+    if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+		peer chaincode instantiate -o orderer.example.com:7050 -C $CHANNEL_NAME -n mycc -v 1.0 -c '{"Args":["init","a","100","b","200"]}' -P "OR ('Org1MSP.peer','Org2MSP.peer')" >&log.txt
+	else
+		peer chaincode instantiate -o orderer.example.com:7050 --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -n mycc -v 1.0 -c '{"Args":["init","a","100","b","200"]}' -P "OR	('Org1MSP.peer','Org2MSP.peer')" >&log.txt
+	fi
+
+	res=$?
+	cat log.txt
+	verifyResult $res "Chaincode instantiation on PEER$PEER on channel '$CHANNEL_NAME' failed"
+	echo "===================== Chaincode Instantiation on PEER$PEER on channel '$CHANNEL_NAME' is successful ===================== "
+	echo
+}
+
+chaincodeQuery () {
+
+    PEER=$1
+    echo "===================== Querying on PEER$PEER on channel '$CHANNEL_NAME'... ===================== "
+    setGlobals $PEER
+
+    local rc=1
+    local starttime=$(date +%s) # 下面循环逻辑的开始时间，单位s
+
+    # continue to poll
+    # 我们要么得到一个成功的回复，要么超时
+    while test "$(($(date +%s)-starttime))" -lt "$TIMEOUT" -a $rc -ne 0
+    do
+        sleep 3
+        echo "Attempting to Query PEER$PEER ...$(($(date +%s)-starttime)) secs"
+
+        peer chaincode query -C $CHANNEL_NAME -n mycc -c '{"Args":["query","a"]}' >&log.txt
+
+        # eg, Query Result: 100
+        test $? -eq 0 && VALUE=$(cat log.txt | awk '/Query Result/ {print $NF}')
+
+        test "$VALUE" = "$2" && let rc=0
+    done
+
+    echo
+    cat log.txt
+
+    if test $rc -eq 0 ; then
+        echo "===================== Query on PEER$PEER on channel '$CHANNEL_NAME' is successful ===================== "
+    else
+        echo "!!!!!!!!!!!!!!! Query result on PEER$PEER is INVALID !!!!!!!!!!!!!!!!"
+        echo "================== ERROR !!! FAILED to execute End-2-End Scenario =================="
+        echo
+        exit 1
+    fi
+}
+
+chaincodeInvoke () {
+
+    PEER=$1
+	setGlobals $PEER
+
+	# while 'peer chaincode' command can get the orderer endpoint from the peer (if join was successful),
+	# lets supply it directly as we know it using the "-o" option
+	if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+		peer chaincode invoke -o orderer.example.com:7050 -C $CHANNEL_NAME -n mycc -c '{"Args":["invoke","a","b","10"]}' >&log.txt
+	else
+		peer chaincode invoke -o orderer.example.com:7050  --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -n mycc -c '{"Args":["invoke","a","b","10"]}' >&log.txt
+	fi
+
+	res=$?
+	cat log.txt
+	verifyResult $res "Invoke execution on PEER$PEER failed "
+	echo "===================== Invoke transaction on PEER$PEER on channel '$CHANNEL_NAME' is successful ===================== "
+	echo
+}
 
 ## 检查orderering服务是否可用
 echo "Check orderering service availability..."
@@ -119,3 +276,53 @@ checkOSNAvailability
 ## 创建应用通道
 echo "Creating channel..."
 createChannel
+
+## 将所有节点加入创建的应用通道
+echo "Having all peers join the channel..."
+joinChannel
+
+## 为应用通道中的每一个组织设置锚节点
+echo "Updating anchor peers for org1..."
+updateAnchorPeers 0
+echo "Updating anchor peers for org2..."
+updateAnchorPeers 2
+
+## 在 Peer0/Org1、Peer1/Org1、Peer0/Org2、Peer1/Org2 节点上安装链码
+echo "Installing chaincode on org1/peer0..."
+installChaincode 0
+echo "Installing chaincode on org1/peer1..."
+installChaincode 1
+echo "Install chaincode on org2/peer0..."
+installChaincode 2
+echo "Install chaincode on org2/peer1..."
+installChaincode 3
+
+# 在 Peer0/Org2 节点上实例化链码
+echo "Instantiating chaincode on org2/peer0..."
+instantiateChaincode 2
+
+# 在Peer0/Org1节点上查询链码，并校验查询结果是否等于实例化链码时指定的值。如果不等于则结束程序
+echo "Querying chaincode on org1/peer0..."
+chaincodeQuery 0 100
+
+# 在Peer0/Org1节点上调用链码
+echo "Sending invoke transaction on org1/peer0..."
+chaincodeInvoke 0
+
+# 在Peer0/Org2节点上查询链码，并校验查询结果是否等于90
+echo "Querying chaincode on org2/peer0..."
+chaincodeQuery 3 90
+
+echo
+echo "===================== All GOOD, End-2-End execution completed ===================== "
+echo
+
+echo
+echo " _____   _   _   ____            _____   ____    _____ "
+echo "| ____| | \ | | |  _ \          | ____| |___ \  | ____|"
+echo "|  _|   |  \| | | | | |  _____  |  _|     __) | |  _|  "
+echo "| |___  | |\  | | |_| | |_____| | |___   / __/  | |___ "
+echo "|_____| |_| \_| |____/          |_____| |_____| |_____|"
+echo
+
+exit 0
